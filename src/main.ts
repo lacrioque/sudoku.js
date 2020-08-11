c7de38e759b1a2fa4ce094acb11f80cf6803e522
lacrioque <markusfluer@markusfluer.de>
2020-08-11 13:24:34 +0200

    feat: added static definitions in closure

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ interface SudokuCollection {
     solver: SudokuSolver;
     getCandidates: SudokuGetCandidates;
     conversions: typeof conversions;
+    statics: Record<string,unknown>;
 }
 
 export default function getSudoku(debug=false): SudokuCollection {
@@ -17,8 +18,19 @@ export default function getSudoku(debug=false): SudokuCollection {
     const generator = new SudokuGenerator(instance,debug);
     const solver = new SudokuSolver(instance,debug);
     const getCandidates = new SudokuGetCandidates(instance,debug);
+    const statics = Object.freeze({
+        DIGITS: Sudoku.DIGITS,
+        ROWS: Sudoku.ROWS,
+        COLS: Sudoku.COLS,
+        DIFFICULTY: Sudoku.DIFFICULTY,
+        BLANK_CHAR: Sudoku.BLANK_CHAR,
+        BLANK_BOARD: Sudoku.BLANK_BOARD,
+        MIN_GIVENS: Sudoku.MIN_GIVENS,
+        NR_SQUARES: Sudoku.NR_SQUARES,
+    });
     
     return {
+        statics,
         instance,
         generator,
         solver,
